38f8d75d2166f3dc7068945c9c83bde2aa767e9d
melek16 <melekchaaben21@gmail.com>
2021-09-18 21:42:48 +0100

    committing update :p

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React,{ Component } from 'react';
+import React from 'react';
 import './App.css';
 import List from './Components/List';
 
-export default class App extends Component{
-    
-  
-  render() {
-    return (
-      <div className="App">
+
+const App = () => {
+  return (
+    <div className="App">
         <h1>Your To Do List</h1>
        <List/>
-  
-      </div>
-    )
-  }
- 
-  
+    </div>
+  )
 }
+
+export default App
+
